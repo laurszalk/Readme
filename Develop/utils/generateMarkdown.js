e8a0fbf86f5c540e5f8af163d76e3aa4a38feca4
laurszalk <laurenszalk@gmail.com>
2023-01-31 09:43:12 -0500

    added to generate markdown function

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,3 +1,5 @@
+const licenseGenerator = require("./licenseGenerator");
+
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
 // return the license badge
